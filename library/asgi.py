065d672c00bdd77703f685764c5b221063d2f236
natkhosh <t9219788607@gmail.com>
2020-04-22 19:48:57 +0300

    ver.05 + create book - update +- svelte

--- library/asgi.py
+++ library/asgi.py
@@ -10,7 +10,21 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from .websocket import websocket_applciation
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library.settings')
 
-application = get_asgi_application()
+django_application = get_asgi_application()
+
+async def application(scope, receive, send):
+    if scope['type'] == 'http':
+        await django_application(scope, receive, send)
+    elif scope['type'] == 'websocket':
+        await websocket_applciation(scope, receive, send)
+    else:
+        raise NotImplementedError(f"Unknown scope type {scope['type']}")
+
+
+# websocket.py
+async def websocket_applciation(scope, receive, send):
+    pass
